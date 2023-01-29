d6d556d54a192f9007d8bb50d9b358ef9941cf40
ZackarySantana <zackzackyack@gmail.com>
2023-01-29 15:54:54 -0500

    Dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), react(), tailwind(), prefetch({
+    selector: "a",
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  })
+});
