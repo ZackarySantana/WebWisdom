89826213359c30139456e7e6c6016821d209bbe3
George <georgeguardia01@gmail.com>
2023-02-02 20:41:33 -0500

    vue-addition

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,22 +10,15 @@ import solidJs from "@astrojs/solid-js";
 import svelte from "@astrojs/svelte";
 import alpinejs from "@astrojs/alpinejs";
 import lit from "@astrojs/lit";
+import vue from "@astrojs/vue";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [
-        preact(),
-        react(),
-        tailwind(),
-        prefetch({
-            selector: "a",
-        }),
-        solidJs(),
-        svelte(),
-        alpinejs(),
-        lit(),
-    ],
-    output: "server",
-    adapter: node({
-        mode: "standalone",
-    }),
+  integrations: [preact(), react(), tailwind(), prefetch({
+    selector: "a"
+  }), solidJs(), svelte(), alpinejs(), lit(), vue({ jsx: true })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
---
+++ src/components/vue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello from Vue {{ counter }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++;
+    }, 1000);
+  },
+};
+</script>
